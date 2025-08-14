27d8ebce9b821ae1a361208e68864c54a26f9371
ibm-webmaster <mahamoudisb@gmail.com>
2025-08-14 22:30:55 +0100

    Commit de l'exo_03

---
+++ exo_03.rb
@@ -0,0 +1,6 @@
+puts "Bonjour, monde !"
+#puts "Et avec une voix sexy, ça donne : Bonjour, monde !"
+puts "En faisant une recherche Google, peux-tu connaître la différence entre print et puts ?"
+
+
+#Avec le # devant la ligne 2 la ligne ne s'affiche pas dans le console, donc à été mis en commentaire
