32599a22bdc34d6a5d295a63d3404e2829eb4fb5
ibm-webmaster <mahamoudisb@gmail.com>
2025-08-15 19:11:20 +0100

    Commit de l'exo 11

---
+++ exo_11.rb
@@ -0,0 +1,14 @@
+puts "Veuillez saisir un chiffre"
+
+print "> "
+
+user_number = gets.chomp.to_i
+
+#puts "Salut, ça farte ?\n"*user_number
+
+#ou
+
+user_number.times do 
+  puts "Salut, ça farte ?"
+end
+
