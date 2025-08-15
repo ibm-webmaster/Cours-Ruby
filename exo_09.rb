f83f2a5c79dd0f646db1acd8b504bdd6dd86791d
ibm-webmaster <mahamoudisb@gmail.com>
2025-08-15 18:31:47 +0100

    Commit de l'exo09

---
+++ exo_09.rb
@@ -0,0 +1,13 @@
+puts "Veuillez saisir votre prénom"
+
+print "> "
+
+user_first_name = gets.chomp
+
+puts "Veuillez saisir votre nom"
+
+print "> "
+
+user_last_name = gets.chomp
+
+puts "Bonjour, #{user_first_name + " " + user_last_name }"
