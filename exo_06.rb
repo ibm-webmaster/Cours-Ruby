408275b2f6e2491bcc4d30e53d1312068b5159b1
ibm-webmaster <mahamoudisb@gmail.com>
2025-08-15 17:49:54 +0100

    Commit de l'exo_06

---
+++ exo_06.rb
@@ -0,0 +1,11 @@
+number_of_hours_worked_per_day = 10
+
+number_of_days_worked_per_week = 5
+
+number_of_weeks_in_TS4A = 11
+
+puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_TS4A}"
+
+puts "Et en minutes ça fait : #{number_of_minutes_in_an_hour * number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_TS4A}"
+
+# Il y a une erreur par ce que la variable "number_of_minutes_in_an_hour" n'a pas été déclaré
