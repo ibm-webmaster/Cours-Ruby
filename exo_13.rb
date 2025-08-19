d344b116c4c63bb065c4e96e197e24cfabe81671
ibm-webmaster <mahamoudisb@gmail.com>
2025-08-19 23:09:09 +0100

    Commit actualisé de l'exo_13

--- exo_13.rb
+++ exo_13.rb
@@ -2,6 +2,8 @@ puts "Entrez votre date de naissance :"
 print "> "
 birth_day = gets.chomp.to_i
 
-for i in birth_day..2025
+current_year = Time.now.year
+
+for i in birth_day..current_year
   puts i
 end
